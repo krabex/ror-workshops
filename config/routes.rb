a7223745401848be873d81d29c94757510c2d2a9
Krzysztof Drab <krabex93@gmail.com>
2015-12-02 20:28:26 +0100

    Added root, reports and teachers routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
     get :subjects
   end
 
+  resources :teachers
+
+  get '/reports/subjects', to: "reports#subjects", as: :report_subjects
+
+  root to: "visitors#index"
+
   devise_for :users 
 end
