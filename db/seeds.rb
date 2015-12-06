c1f4800ab9478d6a04b8f12f151c60cb74cbc7b2
Krzysztof Drab <krabex93@gmail.com>
2015-12-06 14:16:51 +0100

    Prepared seeds with student_payments

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ puts "Seeds: start"
 TEACHER_TITLES = %w(Dr. Prof. TA)
 User.create!(email: 'admin@admin.com',password: 'adminadmin')
 
-30.times do
+3.times do
   Teacher.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -11,14 +11,14 @@ User.create!(email: 'admin@admin.com',password: 'adminadmin')
 end
 
 teachers = Teacher.all
-20.times do
+5.times do
   SubjectItem.create!(
     title: Faker::Lorem.sentence,
     teacher: teachers.sample
   )
 end
 
-40.times do
+25.times do
   Student.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name
@@ -37,6 +37,19 @@ SubjectItem.all.each do |subject_item|
                                                                 subject_item: subject_item,
                                                                 value: rand(1..6))
     end
+  end
+end
+
+students.each do |student|
+  rand(1..3).times do
+    deadline = Faker::Time.between(1.year.ago, 1.month.ago)
+    paid = rand(1..2) == 1    
+    
+    student.student_payments << StudentPayment.create(
+      payment_deadline: deadline,
+      date_of_payment: (paid ? deadline - rand(1..15).days : nil),
+      amount: (paid ? rand(50..100) : 0) 
+    )
   end
 end
 
